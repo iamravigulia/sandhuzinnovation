3aa49b637497e8e3722e8980dd78b8ea1bd69ce4
Ravi Gulia <iamravigulia@users.noreply.github.com>
2023-09-27 23:55:20 +0530

    Update header.php

--- header.php
+++ header.php
@@ -8,6 +8,11 @@
     <title>Sandhuz Innovation</title>
     <!-- <link href="./output.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="./css/style.css">
+    <meta name="description" content="Sandhuz works in Telecommunication and Transport field Australia wide. In telecommunication Sandhuz has worked with Downer, Visionstream, Tech2home, CGA and Hills on FTTC, HFC MDU - SDU, BDOD projects of NBN. Sandhuz also deals in transport and provide safe and reliable service with transport of goods nation-wide." />
+
+<meta name="keywords" content="Sandhuz, Sandhu, Satinder ,INNOVATION" />
+
+<meta name="robots" content="index, follow" />
     <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
